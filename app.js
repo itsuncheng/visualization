cc378cb9b3029f8dfb6245e61efb97cf95deb83d
itsuncheng <itsuncheng2000@gmail.com>
2019-09-11 23:26:20 +0800

    finalized code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const bodyParser= require('body-parser')
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -12,7 +13,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.engine('js', require('ejs').renderFile)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+var similarity = require( 'compute-cosine-similarity' );
+
+function runSimilarity(x,y){
+  var s = similarity( x, y );
+  return s
+}
+
+var wordsPoints;
+
+fs.readFile('./public/javascripts/word3dpoint.json', 'utf8', (err, jsonString) => {
+  if (err) {
+      console.log("File read failed:", err);
+      return;
+  }
+  wordsPoints = JSON.parse(jsonString);
+  // console.log(wordsPoints.word3dpoint);
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Word Embeddings', wordsPoints:wordsPoints});
 });
+
+router.post('/searchWord', (req, res) => {
+  console.log("post")
+  var firstWordPosList = req.body.firstWordPosList
+  var secondWordPosList = req.body.secondWordPosList
+  var similarity = runSimilarity(firstWordPosList, secondWordPosList)
+  
+  res.contentType('json')
+  res.send({"similarity": similarity})
+  // console.log(JSON.stringify(req.body))
+})
 
 module.exports = router;
